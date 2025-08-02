419dbf0fdd526603504a6e9f095cae570cd11c6f
jonathanbbiles <jonathanbbiles@gmail.com>
2025-08-02 15:55:56 -0700

    feat: configure env handling for app

--- BullishorBust/Frontend/app.config.js
+++ BullishorBust/Frontend/app.config.js
@@ -1,13 +1,13 @@
 import 'dotenv/config';
-
-export default ({ config }) => ({
-  ...config,
-  extra: {
-    ...config.extra,
-    EXPO_PUBLIC_ALPACA_KEY: process.env.ALPACA_API_KEY,
-    EXPO_PUBLIC_ALPACA_SECRET: process.env.ALPACA_SECRET_KEY,
-    EXPO_PUBLIC_ALPACA_BASE_URL: process.env.ALPACA_BASE_URL,
-    EXPO_PUBLIC_ALPACA_DATA_URL: process.env.ALPACA_DATA_URL,
-    EXPO_PUBLIC_BACKEND_URL: process.env.EXPO_PUBLIC_BACKEND_URL,
-  },
-});
+export default {
+  expo: {
+    name: "bullish-or-bust",
+    version: "1.0.0",
+    extra: {
+      EXPO_PUBLIC_BACKEND_URL: process.env.EXPO_PUBLIC_BACKEND_URL,
+      EXPO_PUBLIC_ALPACA_KEY: process.env.ALPACA_API_KEY,
+      EXPO_PUBLIC_ALPACA_SECRET: process.env.ALPACA_SECRET_KEY,
+      EXPO_PUBLIC_ALPACA_BASE_URL: process.env.ALPACA_BASE_URL
+    }
+  }
+};
